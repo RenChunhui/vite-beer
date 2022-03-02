27af8bff7e24b1caf87c7256112689535469311c
renchunhui <renchunhui2008@gmail.com>
2022-03-02 20:57:00 +0800

    feat(script): hooks

--- scripts/hooks/after_install.js
+++ scripts/hooks/after_install.js
@@ -1,31 +1,36 @@
 #!/usr/bin/env node
 
-import { execSync, exec } from "child_process"
+const { execSync, exec } = require('child_process')
 
 const extensions = [
-    { id: 'johnsoncodehk.volar', desc: 'Volar' },
-    { id: 'johnsoncodehk.vscode-typescript-vue-plugin', desc: 'TypeScript Vue Plugin' },
-    { id: 'esbenp.prettier-vscode', desc: 'Prettier' },
-    { id: 'bradlc.vscode-tailwindcss', desc: 'Tailwind CSS IntelliSense' }
+  { id: 'johnsoncodehk.volar', desc: 'Volar' },
+  { id: 'johnsoncodehk.vscode-typescript-vue-plugin', desc: 'TypeScript Vue Plugin' },
+  { id: 'esbenp.prettier-vscode', desc: 'Prettier' },
+  { id: 'bradlc.vscode-tailwindcss', desc: 'Tailwind CSS IntelliSense' },
 ]
 
-const installed = Buffer.from(execSync('code --list-extensions'), 'utf-8').toString().split(/[\s\n]/)
+const installed = Buffer.from(execSync('code --list-extensions'), 'utf-8')
+  .toString()
+  .split(/[\s\n]/)
 
 for (const extension of extensions) {
-    if (installed.indexOf(extension.id) === -1) {
-        execSync(`code --install-extension ${extension.id}`)
-        console.log(`${chalk.green('✔')} 成功安装 ${extension.desc}`)
-    } else {
-        console.log(`${chalk.green('✔')} ${chalk.white(extension.desc + '已安装')}`)
-    }
+  if (installed.indexOf(extension.id) === -1) {
+    execSync(`code --install-extension ${extension.id}`)
+    console.log(`✔ 成功安装 ${extension.desc}`)
+  } else {
+    console.log(`✔ ${extension.desc} 已安装`)
+  }
 }
 
-console.log(`${chalk.green('✔')} 完成插件安装`)
+console.log(`✔ 完成插件安装`)
 
-exec('pm', (error, stdout, stderr) => {
-    if (error) {
-        execSync('npm i -g', { stdio: 'inherit' })
-        console.log(`${chalk.green('✔')} 成功安装 sem cli`)
-        return
-    }
+const { bin } = require('./../../package.json')
+const binName = bin.replace('./scripts/bin/', '')
+
+exec(binName, (error, stdout, stderr) => {
+  if (error) {
+    execSync('npm i -g', { stdio: 'inherit' })
+    console.log(`✔ 成功安装 ${binName} cli`)
+    return
+  }
 })
--- scripts/hooks/before_install.js
+++ scripts/hooks/before_install.js
@@ -1,34 +1,55 @@
 #!/usr/bin/env node
 
-import fs from 'fs'
-import path from 'path'
+const fs = require('fs')
+const path = require('path')
 
 // 创建本地 .npmrc
-fs.access('.npmrc', err => {
-    if (err && err.code === 'ENOENT') {
-      fs.writeFile(path.join(process.cwd(), '.npmrc'), 'registry=https://registry.npmmirror.com', () => {
-        console.log(`✔ 成功创建本地 .npmrc`)
-      })
-    }
-  })
-  
-  // 创建本地 .env.local
-  fs.access('.env.local', err => {
-    if (err && err.code === 'ENOENT') {
-      fs.writeFile(path.join(process.cwd(), '.env.local'), '', () => {
-        console.log(`✔ 成功创建本地 .env.local`)
-      })
-    }
-  })
-  
-  // 配置 VSCode，主要是解决了 vetur 与 volar 的冲突
-  fs.rmSync(`${process.cwd()}/.vscode`, { recursive: true, force: true }, err => { })
-  fs.mkdirSync(`${process.cwd()}/.vscode`)
-  fs.writeFileSync(`${process.cwd()}/.vscode/extensions.json`,
+fs.access('.npmrc', (err) => {
+  if (err && err.code === 'ENOENT') {
+    fs.writeFile(path.join(process.cwd(), '.npmrc'), 'registry=https://registry.npmmirror.com', () => {
+      console.log(`✔ 成功创建本地 .npmrc`)
+    })
+  }
+})
+
+// 创建本地 .env.local
+fs.access('.env.local', (err) => {
+  if (err && err.code === 'ENOENT') {
+    fs.writeFile(path.join(process.cwd(), '.env.local'), '', () => {
+      console.log(`✔ 成功创建本地 .env.local`)
+    })
+  }
+})
+
+// 配置 VSCode，主要是解决了 vetur 与 volar 的冲突
+fs.rmSync(`${process.cwd()}/.vscode`, { recursive: true, force: true }, (err) => {})
+fs.mkdirSync(`${process.cwd()}/.vscode`)
+fs.writeFileSync(
+  `${process.cwd()}/.vscode/settings.json`,
+  `
+  {
+    "editor.formatOnSave": true,
+    "[javascript]": {
+      "editor.defaultFormatter": "esbenp.prettier-vscode"
+    },
+    "[typescript]": {
+      "editor.defaultFormatter": "esbenp.prettier-vscode"
+    },
+    "[vue]": {
+      "editor.defaultFormatter": "esbenp.prettier-vscode"
+    },
+    "css.lint.unknownAtRules": "ignore"
+  }
+  `
+)
+fs.writeFileSync(
+  `${process.cwd()}/.vscode/extensions.json`,
   `{
-    "recommendations": ["johnsoncodehk.volar"],
+    "recommendations": ["johnsoncodehk.volar", "johnsoncodehk.vscode-typescript-vue-plugin"]
     "unwantedRecommendations": ["octref.vetur"]
   }
-  `, () => {
+  `,
+  () => {
     console.log(`✔ 成功配置本地 vscode`)
-  })
+  }
+)
